10cc302b480fc3ff72054aebd6dda1f57ef63ccd
PierreBader <34540557+PierreBader@users.noreply.github.com>
2024-09-26 14:15:52 +0000

    Affichage des bénévoles en ligne

--- src/components/PlanningCell.vue
+++ src/components/PlanningCell.vue
@@ -4,14 +4,14 @@
     :class="{ selectableCell, selectedCell }"
     @click="cellClick"
   >
-    <div
+    <span
       v-for="benevole in benevoles"
       :key="benevole"
       class="benevole"
       :class="{ selected: benevole == guiStore.selectedBenevole }"
     >
       {{ benevole }}
-    </div>
+    </span>
   </div>
 </template>
 
@@ -113,8 +113,9 @@ function cellClick() {
   margin: 0.2em;
 
   .benevole {
+    flex: 1;
     margin-right: 0.5em;
-    white-space: nowrap;
+    display: inline-block;
   }
 }
 
